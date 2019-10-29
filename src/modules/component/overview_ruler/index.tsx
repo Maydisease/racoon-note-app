f02a4e216bde2a3eaa80decff1abc4992438e93c
tandongsheng <medivh@jollycorp.com>
2019-10-29 18:40:15 +0800

    fix: 修复 overview_ruler 因滚动条滚动后，计算偏移的BUG

--- src/modules/component/overview_ruler/index.tsx
+++ src/modules/component/overview_ruler/index.tsx
@@ -14,6 +14,8 @@ class OverviewRuler extends React.Component {
 
     public props: DefaultProps;
 
+    public drawMarkTimer: number;
+
     constructor(props: any) {
         super(props);
         this.drawElementRef = React.createRef();
@@ -30,7 +32,10 @@ class OverviewRuler extends React.Component {
         }
 
         storeSubscribe('NOTE$QUICK_SEARCH', (action: any) => {
-            this.drawMark();
+            clearTimeout(this.drawMarkTimer);
+            window.setTimeout(() => {
+                this.drawMark();
+            }, 200);
         });
 
         storeSubscribe('NOTE$UN_SEARCH_TAG', (action: any) => {
@@ -54,20 +59,24 @@ class OverviewRuler extends React.Component {
         if (!this.drawElementVmContext) {
             this.drawElementVm        = this.drawElementRef.current as HTMLCanvasElement;
             this.drawElementVmContext = this.drawElementVm.getContext("2d") as CanvasRenderingContext2D;
+            this.drawElementVmContext.clearRect(0, 0, this.drawElementVm.width, this.drawElementVm.height);
         }
-
-        this.drawElementVm.width            = this.drawElementVm.clientWidth;
-        this.drawElementVm.height           = this.drawElementVm.clientHeight;
-        const listenElement                 = this.props.listenRef.current as HTMLDivElement;
-        const listenRefScrollHeight         = listenElement.scrollHeight;
-        const listenClientHeight            = listenElement.clientHeight;
-        let zoomRatio                       = listenClientHeight / listenRefScrollHeight;
-        zoomRatio                           = Number(zoomRatio.toFixed(2));
-        this.drawElementVmContext.fillStyle = "RGB(255, 0, 0, 1)";
         setTimeout(() => {
+            this.clearDraw();
+            this.drawElementVm.width            = this.drawElementVm.clientWidth;
+            this.drawElementVm.height           = this.drawElementVm.clientHeight;
+            const listenElement                 = this.props.listenRef.current as HTMLDivElement;
+            const listenRefScrollHeight         = listenElement.scrollHeight;
+            const listenClientHeight            = listenElement.clientHeight;
+            const lisentGetBoundingClientRect   = listenElement.getBoundingClientRect();
+            let zoomRatio                       = listenClientHeight / listenRefScrollHeight;
+            zoomRatio                           = Number(zoomRatio.toFixed(2));
+            this.drawElementVmContext.fillStyle = "RGB(255, 0, 0, 1)";
+
             const markElements = listenElement.querySelectorAll('.sch-highlight');
             markElements.forEach((element: any) => {
-                this.drawElementVmContext.fillRect(0, element.offsetTop * zoomRatio, this.drawElementVm.width, 1);
+                const y = element.getBoundingClientRect().top + listenElement.scrollTop - lisentGetBoundingClientRect.top;
+                this.drawElementVmContext.fillRect(0, y * zoomRatio, this.drawElementVm.width, 1);
             })
         });
 
