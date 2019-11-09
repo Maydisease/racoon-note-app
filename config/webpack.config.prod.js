f75214389dfde0751d282bc8e541ba58e6c14f6d
tandongsheng <medivh@jollycorp.com>
2019-11-09 22:24:16 +0800

    test: 调试monaco-editor生产环境

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -357,7 +357,7 @@ module.exports = {
 			tsconfig: paths.appTsProdConfig,
 			tslint: paths.appTsLint
 		}),
-		new MonacoWebpackPlugin()
+//		new MonacoWebpackPlugin()
 	],
 	// Some libraries import Node modules but don't use them in the browser.
 	// Tell Webpack to provide empty mocks for them so importing them works.
