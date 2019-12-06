a39c402f785dbb516a4f5673da44aa362cfa45ff
Maydisease <tandongs@qq.com>
2019-12-06 16:59:25 +0800

    test: debug线上文章标题保存混乱的问题2

--- src/modules/component/editors/monaco/index.tsx
+++ src/modules/component/editors/monaco/index.tsx
@@ -69,19 +69,23 @@ class EditorMonaco extends React.Component {
     }
 
     public updateArticleStore(immediatelyUpdate: boolean = false): boolean | void {
-        // 清除已有的定时器
-        if (this.changeContentTimer) {
-            clearTimeout(this.changeContentTimer);
-        }
+
         // 如果需要立即更新的话
         if (immediatelyUpdate) {
-            this.writeTempArticleStore();
+            this.changeContentTimer = window.setTimeout(() => {
+                this.writeTempArticleStore();
+            }, 10);
             return false;
+        } else {
+            // 清除已有的定时器
+            if (this.changeContentTimer) {
+                clearTimeout(this.changeContentTimer);
+            }
+            // 正常逻辑的更新
+            this.changeContentTimer = window.setTimeout(() => {
+                this.writeTempArticleStore();
+            }, this.changeContentIntervalTime);
         }
-        // 正常逻辑的更新
-        this.changeContentTimer = window.setTimeout(() => {
-            this.writeTempArticleStore();
-        }, this.changeContentIntervalTime);
     }
 
     // 设置编辑器的认值
@@ -120,6 +124,7 @@ class EditorMonaco extends React.Component {
         storeSubscribe('WINDOW_KEYBOARD$CMD_OR_CTRL_S', async () => {
             const editMode = store.getState().STORE_NOTE$FRAME.editMode;
             if (editMode) {
+                this.updateArticleStore(true);
                 await new ArticleService().saveNote();
             }
         });
