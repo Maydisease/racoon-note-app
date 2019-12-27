f3c34b8d0307662de75dccc1a6bab14be8195a38
Maydisease <tandongs@qq.com>
2019-12-27 11:44:33 +0800

    feat: 优化light-box阶段一

--- src/modules/component/light_box/index.tsx
+++ src/modules/component/light_box/index.tsx
@@ -2,7 +2,12 @@ import './light_box.scss';
 import * as ReactDOM from "react-dom";
 import * as React    from "react";
 
+const openseadragon = require('openseadragon');
+
 class Component extends React.Component {
+
+    public lightBoxElementRef: React.RefObject<HTMLDivElement>;
+    public viewer: any;
 
     public props: any = {
         handleClose: Function,
@@ -11,23 +16,96 @@ class Component extends React.Component {
 
     constructor(props: any) {
         super(props);
-        this.props       = props;
-        this.handleClose = this.handleClose.bind(this);
+        this.props              = props;
+        this.handleClose        = this.handleClose.bind(this);
+        this.viewResetHandle    = this.viewResetHandle.bind(this);
+        this.viewCloseHandle    = this.viewCloseHandle.bind(this);
+        this.lightBoxElementRef = React.createRef();
     }
 
     public componentDidMount() {
+        const imageElement  = new Image();
+        imageElement.src    = this.props.imageUrl;
+        imageElement.onload = () => {
+            const lightBoxElement = this.lightBoxElementRef.current;
+            this.viewer           = openseadragon({
+                element          : lightBoxElement,
+                tileSources      : {
+                    type     : 'image',
+                    prefixUrl: 'canvasImageDrag',
+                    url      : this.props.imageUrl,
+                    size     : {
+                        width : imageElement.width,
+                        height: imageElement.height
+                    }
+                },
+                debugMode        : false,
+                autoHideControls : true,
+                defaultZoomLevel : .5,
+                autoResize       : true,
+                minLevel         : .5,
+                visibilityRatio  : 1,
+                minZoomImageRatio: .5,
+                maxZoomImageRatio: 1,
+                zoomPerClick     : 1,
+                zoomInButton     : "light-box-image-zoom-in",
+                zoomOutButton    : "light-box-image-zoom-out"
+            });
+
+            // let flag = false;
+            // this.viewer.addHandler('zoom', (eventSource: any) => {
+            //     console.log(eventSource.zoom, this.viewer.minZoomImageRatio, flag);
+            //     if (eventSource.zoom < this.viewer.minZoomImageRatio) {
+            //         if (!flag) {
+            //             flag             = true;
+            //             eventSource.zoom = this.viewer.minZoomImageRatio;
+            //             this.viewResetHandle();
+            //         }
+            //     } else {
+            //         flag = false;
+            //     }
+            //     console.log(this.viewer.minZoomImageRatio);
+            // });
+
+            this.viewer.addHandler('open', (v: any) => {
+                const source    = v.source;
+                const imageSize = source.size;
+                console.log(imageSize);
+
+                const tiledImage = this.viewer.world.getItemAt(0); // Assuming you just have a single image in the viewer
+                const targetZoom = tiledImage.source.dimensions.x / this.viewer.viewport.getContainerSize().x;
+                this.viewer.viewport.zoomTo(targetZoom, null, false);
+            });
+        };
+
         this.props.onRef(this);
     }
 
+    public viewResetHandle() {
+        const tiledImage = this.viewer.world.getItemAt(0); // Assuming you just have a single image in the viewer
+        const targetZoom = tiledImage.source.dimensions.x / this.viewer.viewport.getContainerSize().x;
+        this.viewer.viewport.zoomTo(targetZoom, null, false);
+    }
+
+    public viewCloseHandle() {
+        this.viewer.close();
+        this.handleClose();
+    }
+
     public handleClose() {
-        this.props.handleClose(this.props.mountContainer)
+        this.props.handleClose(this.props.mountContainer);
     }
 
     public render() {
         return (
-            <div id="v-lightBox">
-                <img src={this.props.imageUrl}/>
+            <div id="v-lightBox" ref={this.lightBoxElementRef}>
                 <div className="mask" onClick={this.handleClose}/>
+                <div className="control-bar">
+                    <span id={'light-box-image-zoom-in'}>in</span>
+                    <span id={'light-box-image-zoom-out'}>out</span>
+                    <span onClick={this.viewResetHandle}>reset</span>
+                    <span onClick={this.viewCloseHandle}>close</span>
+                </div>
             </div>
         )
     }
